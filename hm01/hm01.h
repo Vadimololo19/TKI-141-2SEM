de6974cc1ed929090e9343f7a16912db817986f6
Vadimololo19 <vadyst.tkachev@yandex.ru>
2025-03-04 07:26:14 +0300

    home_work_1_REASSEMBLED

--- hm01/hm01.h
+++ hm01/hm01.h
@@ -1,6 +1,4 @@
 #pragma once
-
-#include <iostream>
 #include <string>
 
 class Person
@@ -11,16 +9,10 @@ class Person
         std::string patronymic;
     public:
         Person(std::string ln,std::string fn, std::string pn) : lastName(ln), firstName(fn), patronymic(pn) {}
-        void show()
-        {   
-        std::cout << lastName << " " << firstName << std::endl;
-        }
+        void show();
+        void showFormal();
         //Person(const std::string& ln,const std::string& fn,const std::string& pn = "");
         //void Show();
-        //void ShowFormal();
-        void showFormal()
-        {
-            std::cout << lastName << " " << firstName << " " << patronymic << std::endl;
-        } 
+        //void ShowFormal(); 
 };
 
