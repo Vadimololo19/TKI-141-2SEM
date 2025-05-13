9c95a8e26b78bc1e961f1654ee0c9b3620c3b2d5
Vadimololo19 <vadyst.tkachev@yandex.ru>
2025-05-13 21:37:54 +0300

    fix conflict

--- build/CMakeFiles/TKI-141-2SEM.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TKI-141-2SEM.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/Kaguya/MIIT/Scripts/TKI-141-2SEM
-# Build directory: /home/Kaguya/MIIT/Scripts/TKI-141-2SEM/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "/home/Kaguya/MIIT/Scripts/TKI-141-2SEM/build/tests")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/home/Kaguya/MIIT/Scripts/TKI-141-2SEM/CMakeLists.txt;37;add_test;/home/Kaguya/MIIT/Scripts/TKI-141-2SEM/CMakeLists.txt;0;")
